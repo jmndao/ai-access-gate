f9f7716ed0035bf80a234571d1f172e35f9037ea
jmndao <jmndao@gmail.com>
2023-01-20 00:55:38 +0000

    Add video stream with rsid_py

--- main.py
+++ main.py
@@ -4,36 +4,58 @@
 # main.py
 # import the necessary packages
 from flask import Flask, render_template, Response, request, send_from_directory
-from camera import VideoCamera
-import os
 
-pi_camera = VideoCamera(flip=False) # flip pi camera if upside down.
+import time
+import os
+import threading
+
+import cv2
+import numpy as np
+import rsid_py
+
+frame = None
 
 # App Globals (do not edit)
 app = Flask(__name__)
+
+def on_image(image):
+    buffer = memoryview(image.get_buffer())
+    arr = np.asarray(buffer, dtype=np.uint8)
+    cv2.imwrite('capture.jpg', arr)
+
+class VideoStream:
+    def __init__(self):
+        self.preview_cfg = rsid_py.PreviewConfig()
+        self.preview_cfg.camera_number = -1
+        self.p = rsid_py.Preview(self.preview_cfg)
+        self.p.start(on_image)
+
+    def gen(self):
+        while True:
+            yield(b'--frame\r\n'
+                  b'Content-Type: image/jpeg\r\n\r\n' +  open('capture.jpg','rb').read() + b'\r\n\r\n')
+
+    def __del__(self):
+        self.p.stop()
+
 
 @app.route('/')
 def index():
     return render_template('index.html') #you can customze index.html here
 
-def gen(camera):
-    #get camera frame
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(pi_camera),
+    return Response(VideoStream().gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # Take a photo when pressing camera button
 @app.route('/picture')
 def take_picture():
-    pi_camera.take_picture()
+    # pi_camera.take_picture()
     return "None"
 
 if __name__ == '__main__':
-
-    app.run(host='0.0.0.0', debug=False)
+    try:
+        app.run(host='0.0.0.0', debug=False)
+    finally:
+        VideoStream().p.stop()
