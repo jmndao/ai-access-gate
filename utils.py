3eff91d36f2342ed80f735d03e3cb08c94309893
jmndao <jmndao@gmail.com>
2023-04-16 16:16:10 +0000

    image lock class

---
+++ utils.py
@@ -0,0 +1,9 @@
+class ImageLock:
+    def __init__(self, lock):
+        self.lock = lock
+
+    def __enter__(self):
+        self.lock.acquire()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.lock.release()
